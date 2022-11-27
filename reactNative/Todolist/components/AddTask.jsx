f08f66d9f359fd3291cb11fb3a5b5866c73a6f57
KimSungJin <rainbow96bear@gmail.com>
2022-11-27 21:45:07 +0900

    할 일 추가기능 구현

--- reactNative/Todolist/components/AddTask.jsx
+++ reactNative/Todolist/components/AddTask.jsx
@@ -1,22 +1,82 @@
 import React from 'react';
 import {useState} from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 
-export default function AddTask({route}) {
+export default function AddTask({navigation, route}) {
+  const [task, setTask] = useState('');
+  function addTask(newTask) {
+    if (task === '') {
+      alert('할 일을 입력해주세요.');
+    } else {
+      route.params.setTaskArr([...route.params.taskArr, newTask]);
+      navigation.goBack();
+    }
+  }
+  function cancleAdd() {
+    navigation.goBack();
+  }
   return (
-    <View>
-      <Text>할 일의 제목을 입력하세요.</Text>
+    <View style={styles.base}>
       <TextInput
+        style={styles.inputBox}
         underlineColorAndroid="transparent"
         placeholder="할 일"
         placeholderTextColor="#9a73ef"
         autoCapitalize="none"
-        // onChangeText={titleInput}
-      ></TextInput>
-      {/* <TouchableOpacity onPress={() => testfunc(title)}>
-        <Text>여기 눌러요</Text>
-      </TouchableOpacity> */}
-      <Text>{route.params.test}</Text>
+        value={task}
+        onChangeText={text => {
+          setTask(text);
+        }}></TextInput>
+      <View style={styles.btnBox}>
+        <TouchableOpacity style={styles.btnStyle} onPress={() => cancleAdd()}>
+          <Text>취소</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnStyle} onPress={() => addTask(task)}>
+          <Text>할 일 추가</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  base: {
+    flex: 1,
+    padding: 10,
+    display: 'flex',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  inputBox: {
+    width: '90%',
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#9a73ef',
+    borderStyle: 'solid',
+  },
+  btnBox: {
+    width: '90%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    bottom: 20,
+  },
+  btnStyle: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '40%',
+    backgroundColor: '#9a73ef',
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#9a73ef',
+    borderStyle: 'solid',
+  },
+});
--- reactNative/Todolist/components/Main.jsx
+++ reactNative/Todolist/components/Main.jsx
@@ -5,37 +5,64 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  Dimensions,
+  ScrollView,
 } from 'react-native';
-import menuBar from '../img/menuBar.png';
 import plus from '../img/plus.png';
-export default function Main({navigation}) {
-  const [taskArr, setTaskArr] = useState('안녕');
+export default function Main({navigation, route}) {
+  const [taskArr, setTaskArr] = useState(['할 일']);
   return (
-    <View style={styles.topBanner}>
-      <TouchableOpacity>
-        <Image source={menuBar} style={styles.topBannerBtn}></Image>
-      </TouchableOpacity>
-      <Text>Todo List</Text>
+    <View style={styles.base}>
+      <ScrollView style={styles.taskBox}>
+        {taskArr.map((item, index) => (
+          <Text style={styles.taskItem}>
+            {index + 1}. {item}
+          </Text>
+        ))}
+      </ScrollView>
       <TouchableOpacity
-        onPress={() => navigation.navigate('AddTask', {test: taskArr})}>
-        <Image source={plus} style={styles.topBannerBtn}></Image>
+        style={styles.addTaskBtn}
+        onPress={() =>
+          navigation.navigate('AddTask', {
+            taskArr,
+            setTaskArr,
+          })
+        }>
+        <Image source={plus} style={styles.addTaskBtnImg}></Image>
       </TouchableOpacity>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  topBanner: {
-    padding: 5,
-    backgroundColor: 'lightblue',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  base: {
+    flex: 1,
+    padding: 10,
+    position: 'relative',
   },
-  topBannerBtn: {
+  addTaskBtn: {
+    padding: 13,
+    backgroundColor: '#9a73ef',
+    position: 'absolute',
+    right: 35,
+    bottom: 35,
+    borderWidth: 2,
+    borderRadius: 30,
+    borderColor: '#9a73ef',
+    borderStyle: 'solid',
+  },
+  addTaskBtnImg: {
     width: 30,
     height: 30,
+  },
+  taskBox: {
+    paddingBottom: 10,
+  },
+  taskItem: {
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 10,
+    borderWidth: 2,
+    borderRadius: 20,
+    borderColor: '#9a73ef',
+    borderStyle: 'solid',
   },
 });
