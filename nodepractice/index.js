c3cd272cad5f8dd4ee8c801ea1b83e98674dce51
KimSungJin <rainbow96bear@gmail.com>
2023-03-02 15:16:03 +0900

    정리

---
+++ nodepractice/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
+
+const app = express();
+
+app.set("port", process.env.PORT || 8080);
+app.use("/", express.static(path.join(__dirname, "main")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV == "production") {
+    morgan("combined")(req, res, next);
+  } else {
+    morgan("dev")(req, res, next);
+  }
+});
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    secret: process.env.COOKIE_SECRET,
+    name: "session",
+  })
+);
+app.listen(app.get("port"), () => {
+  console.log("서버 열음");
+});
